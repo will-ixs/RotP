5e1b2155df1345d7334361b7a71a8babe3142bef
Fredy-Medawar <fredymedawar@gmail.com>
2024-02-07 19:10:31 -0600

    Added indicators for health changes

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -11,10 +11,26 @@ public class PlayerHealth : MonoBehaviour
     private float curHealth;
     public GameObject indicator;
 
+    private Transform canvasTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialize default values
+        curHealth = maxHealth;
+        healthBar.maxValue = maxHealth;
+        healthBar.value = maxHealth;
+        healthText.text = curHealth + "/" + maxHealth;
+
+        canvasTransform = GameObject.Find("Canvas").transform;
+    }
+
+    // Updates player health and UI by amount
     public void updatePlayerHealth(float amount)
     {
         curHealth += amount;
 
+        // Prevent negative health
         if (curHealth < 0)
         {
             curHealth = 0;
@@ -23,15 +39,26 @@ public class PlayerHealth : MonoBehaviour
         // Update UI to reflect health
         healthBar.value = curHealth;
         healthText.text = curHealth + "/" + maxHealth;
-    }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        curHealth = maxHealth;
-        healthBar.maxValue = maxHealth;
-        healthBar.value = maxHealth;
-        healthText.text = curHealth + "/" + maxHealth;
+        GameObject healthIndicator = Instantiate(indicator);
+
+        // Set text to display above player
+        Text healthIndicatorText = healthIndicator.GetComponent<Text>();
+
+        healthIndicatorText.text = amount.ToString();
+        if (amount > 0)
+        {
+            healthIndicatorText.text = "+" + amount.ToString();
+        }
+
+        // Display text above player
+        healthIndicator.transform.SetParent(canvasTransform, false);
+        healthIndicator.transform.position = Camera.main.WorldToScreenPoint(transform.position);
+        healthIndicator.transform.position = new Vector3(healthIndicator.transform.position.x + 15f + Random.Range(-25f, 25f), healthIndicator.transform.position.y + 100f + Random.Range(-20f, 10f), healthIndicator.transform.position.z);
+
+        // Remove text after delay
+        Destroy(healthIndicator, 1.0f);
+
     }
 
     // Update is called once per frame
