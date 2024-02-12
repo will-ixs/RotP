0a212c33773fb8e2d33d778bf09717d2a8bddc94
Will <109585548+will-ixs@users.noreply.github.com>
2024-02-12 16:42:23 -0600

    Update SerpopardSpit.cs

--- Assets/Scripts/SerpopardSpit.cs
+++ Assets/Scripts/SerpopardSpit.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class SerpopardSpit : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject player;
+    private bool spitting = false;
+    private float spitAngleDelta;
+    [SerializeField] private float spitCooldown;
+    private float useCooldown;
+    private float dmg = 12.0f;
+    [SerializeField] private Animator anim;
+    [SerializeField] private int iterations;
+    private float angleToPlayer;
+    [SerializeField] GameObject spitPrefab;
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    public void Disable()
+    {
+        spitting = false;
     }
 
-    // Update is called once per frame
+    public void Enable()
+    {
+        spitting = true;
+        //Find angle to player
+        Vector3 vectorToPlayer = player.transform.position - transform.position;
+        float angleToPlayer = Mathf.Atan2(vectorToPlayer.y, vectorToPlayer.x) * Mathf.Rad2Deg;
+    }
+
     void Update()
     {
-        
+        useCooldown -= Time.deltaTime;
+        if(spitting)
+        {
+            if(useCooldown <= 0.0f){
+                Spit();
+                useCooldown = spitCooldown;
+            }
+        }
     }
+
+    private void Spit()
+    {
+        /*
+        for(int i = 0; i < iterations; i++){
+            Instantiate(spitPrefab, transform.position + ve, Quaternion.Euler());
+Declaration
+        }*/
+    }
+
 }
