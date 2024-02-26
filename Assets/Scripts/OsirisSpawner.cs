5c287c0c809517df3b6693bfb4cebbb92eaccdd2
jacoby0729 <jacoby0729@gmail.com>
2024-02-26 16:40:57 -0600

    Added OsirisSpawner Script

---
+++ Assets/Scripts/OsirisSpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OsirisSpawner : MonoBehaviour
+{
+
+    private GameObject player;
+
+    [SerializeField] private Transform spawnLocation;
+    [SerializeField] private float healthThreshold;
+    [SerializeField] private GameObject enemyPrefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerHealth playerhealth = collision.gameObject.GetComponent<PlayerHealth>();
+        
+    }
+}
