50343142699ec7b8e143258e8a9ef9673af9c820
Will <109585548+will-ixs@users.noreply.github.com>
2024-02-28 17:09:07 -0600

    Crypt State

--- Assets/Scripts/CryptProgressionManager.cs
+++ Assets/Scripts/CryptProgressionManager.cs
@@ -6,23 +6,91 @@ public class CryptProgressionManager : MonoBehaviour
 {
     public enum CryptState
     {
-        Enemies,
-        EnemiesAndBoss,
-        CleanupEnemies
+        Tomb,
+        HallwayChaos,
+        HallwayCalm,
+        BossFight
     }
-    public List<Transform> stageOneSpawners = new List<Transform>();
+    public List<EnemySpawner> tombSpawners = new List<EnemySpawner>();
+    public List<EnemySpawner> hallwaySpawners = new List<EnemySpawner>();
+    public List<EnemySpawner> bossSpawners = new List<EnemySpawner>();
     public GameObject PurpleSerpopard;
     public GameObject YellowSerpopard;
 
+    private CryptState currState;
+    private CryptState nextState;
 
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(nextState != currState){
+            switch(currState)
+            {
+                case CryptState.Tomb:
+                    ActivateTombSpawners();
+                    break;
+                case CryptState.HallwayChaos:
+                    ActivateHallwaySpawners();
+                    break;
+                case CryptState.HallwayCalm:
+                    DisableSpawners();
+                    break;
+                case CryptState.BossFight:
+                    ActivateBossSpawners();
+                    break;
+            }
+        }
+    }
+
+    private void ActivateTombSpawners(){
+        foreach(EnemySpawner e in hallwaySpawners){
+            e.DisableSpawning();
+        }
+        foreach(EnemySpawner e in bossSpawners){
+            e.DisableSpawning();
+        }
+        foreach(EnemySpawner e in tombSpawners){
+            e.EnableSpawning();
+        }
+    }
+
+    private void ActivateHallwaySpawners(){
+        foreach(EnemySpawner e in tombSpawners){
+            e.DisableSpawning();
+        }
+        foreach(EnemySpawner e in bossSpawners){
+            e.DisableSpawning();
+        }
+        foreach(EnemySpawner e in hallwaySpawners){
+            e.EnableSpawning();
+        }
+    }
+
+    private void ActivateBossSpawners(){
+        foreach(EnemySpawner e in tombSpawners){
+            e.DisableSpawning();
+        }
+        foreach(EnemySpawner e in hallwaySpawners){
+            e.DisableSpawning();
+        }
+        foreach(EnemySpawner e in bossSpawners){
+            e.EnableSpawning();
+        }
+    }
+
+    private void DisableSpawners(){
+        foreach(EnemySpawner e in tombSpawners){
+            e.DisableSpawning();
+        }
+        foreach(EnemySpawner e in hallwaySpawners){
+            e.DisableSpawning();
+        }
+        foreach(EnemySpawner e in bossSpawners){
+            e.DisableSpawning();
+        }
     }
 }
