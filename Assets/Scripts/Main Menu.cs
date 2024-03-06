fbabdd9727a08859bb2657a6462b2330a4b67b4b
Fredy-Medawar <fredymedawar@gmail.com>
2024-03-06 11:11:20 -0600

    Added main menu scene

---
+++ Assets/Scripts/Main Menu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour { 
+    public Button play;
+    public Button levels;
+    public Button options;
+    public Button exit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        play.onClick.AddListener(Play);
+        levels.onClick.AddListener(Levels);
+        options.onClick.AddListener(Options);
+        exit.onClick.AddListener(Exit);
+    }
+
+    void Play()
+    {
+        SceneManager.LoadScene("Crypt");
+    }
+
+    void Levels()
+    {
+
+    }
+
+    void Options()
+    {
+
+    }
+
+    void Exit()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
